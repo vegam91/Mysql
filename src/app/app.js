8648f92281d233cca2d5194e9ed3b46b23627e88
vegam91 <vegamartinoj@gmail.com>
2023-12-17 20:11:54 +0100

    fix: post

--- src/app/app.js
+++ src/app/app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("this is my app ");
 });
-
+app.use(express.json())
 app.use("/api/v1", router);
 
 module.exports = app;
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -3,6 +3,22 @@ const { faker } = require("@faker-js/faker");
 
 const Products = require("../model/product.model");
 
+router.post("/products", async (req, res) => {
+  const dataProducts = req.body
+  await Products.sync();
+  const createProduct = await Products.create({
+    product_name: dataProducts.product_name,
+    price: dataProducts.price,
+    is_stock:  dataProducts.is_stock,
+  });
+  res.status(201).json({
+    ok: true,
+    status: 201,
+    message: "Product Created",
+  }); console.log("PRODUCTO NUEVO", createProduct )
+});
+
+
 router.get("/products", async (req, res) => {
  const products = await  Products.findAll()
  res.status(200).json({
@@ -12,26 +28,24 @@ router.get("/products", async (req, res) => {
  })
 });
 
-router.get("/products", (req, res) => {
-  res.send("I'm a router ");
-});
-
-router.post("/products", async (req, res) => {
-  await Products.sync();
-  const createProduct = await Products.create({
-    product_name: faker.commerce.product(),
-    price: faker.commerce.price(),
-    is_stock: faker.datatype.boolean(),
+router.get("/products/:products_id", async (req, res) => {
+  const id = req.params.products_id
+  const product = await  Products.findOne({
+      where: {product_id :id}
   });
-  res.status(201).json({
-    ok: true,
-    status: 201,
-    message: "Product Created",
-  }); console.log("PRODUCTO NUEVO", createProduct )
-});
 
-router.put("/products", (req, res) => {
-  res.send("I'm a router ");
+ res.status(200).json({
+  ok:true,
+  status:200,
+  body: product,
+ })
+})
+
+
+
+router.put("/products", async (req, res) =>  {
+  const updateProduct = await Products.update()
+
 });
 
 router.delete("/products", (req, res) => {
