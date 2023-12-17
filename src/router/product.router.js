5940ed163a24fb456250c4add41d6604900e74a7
vegam91 <vegamartinoj@gmail.com>
2023-12-17 18:59:09 +0100

    add:getall router

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -3,8 +3,13 @@ const { faker } = require("@faker-js/faker");
 
 const Products = require("../model/product.model");
 
-router.get("/products", (req, res) => {
-  res.send("I'm a router ");
+router.get("/products", async (req, res) => {
+ const products = await  Products.findAll()
+ res.status(200).json({
+  ok:true,
+  status:200, 
+  body: products
+ })
 });
 
 router.get("/products", (req, res) => {
@@ -14,7 +19,7 @@ router.get("/products", (req, res) => {
 router.post("/products", async (req, res) => {
   await Products.sync();
   const createProduct = await Products.create({
-    product_name: faker.commerce.productName(),
+    product_name: faker.commerce.product(),
     price: faker.commerce.price(),
     is_stock: faker.datatype.boolean(),
   });
